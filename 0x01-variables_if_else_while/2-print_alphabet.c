58dce65d01f8941ddd4801de5e1c1bc8345e30a1
Olani Baissa <olanibaissa03@gmail.com>
2021-08-27 12:31:40 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main -  prints the alphabet in lowercase
+ * description - output depends on the condition
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char ch = 'a';
+(ch='a'; ch<='z'; ch++)
+putchar(ch);
+putchar('\n');
+return 0;
+}
