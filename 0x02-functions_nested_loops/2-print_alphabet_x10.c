fed8af896d1425c9deaa7c9d17137c92765b2c0d
Olani Baissa <olanibaissa03@gmail.com>
2021-09-01 09:57:01 -0700

    0x02-functions_nested_loops

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -7,9 +7,11 @@
 
 void print_alphabet_x10(void)
 {
-for (int i = 0; i < 10; i++)
+int i;
+char j;  
+for ( i = 0; i < 10; i++)
 {
-for (char j = 'a'; c <= 'z'; c++)
+for ( j = 'a'; c <= 'z'; c++)
 {
 _putchar(c);
 }
