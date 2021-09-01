e03134b6ba94ca8ee63ae403fb9630287d63a9dd
Olani Baissa <olanibaissa03@gmail.com>
2021-09-01 12:30:32 -0700

    0x02-functions_nested_loops

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,16 +1,16 @@
 #include "main.h"
 /**
- * main - check the code for ALX School students.
- *
- * Return: Always 0.
- */
+* print_alphabet - print lowercase alphabet with c incrementing by 1
+* Return: lowercase alphabet a-z
+*/
 void print_alphabet(void)
 {
-char i;  
-for ( i = 'a'; i  <= 'z'; i++)
+char c;
+c = 'a';
+while (c <= 'z')
 {
-_putchar (i);
-  
+_putchar (c);
+c++;
 }
 _putchar('\n');
 }
--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,19 +1,17 @@
-#include "main.h"
+#include "main"
 /**
- * main - check the code for ALX School students.
- *
- * Return: Always 0.
- */
-
+* print_alphabet_x10 - prints alphabet 10 times from a to z
+* Return: the lowercase alphabet
+*/
 void print_alphabet_x10(void)
 {
+char c;
 int i;
-char j;  
-for ( i = 0; i < 10; i++)
+for (i = 0; i < 10; i++)
 {
-for ( j = 'a'; j <= 'z'; j++)
+for (c = 'a'; c <= 'z'; c++)
 {
-_putchar(j);
+_putchar(c);
 }
 _putchar('\n');
 }
